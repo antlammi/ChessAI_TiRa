d70e45196fba5dab15977fe6eb684eab9a6a097d
antlammi <antti.lammi@helsinki.fi>
2019-04-28 23:35:17 +0300

    More work on connection -- is now able to make a first move in xboard but not other moves

--- src/main/java/kufbot/connection/Connection.java
+++ src/main/java/kufbot/connection/Connection.java
@@ -5,11 +5,13 @@
  */
 package kufbot.connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 import kufbot.engine.MinmaxAB;
 import kufbot.model.Move;
 import kufbot.model.*;
@@ -24,15 +26,18 @@ public class Connection {
     private Player b;
     private MinmaxAB engine;
     private Square[][] state;
-
+    private BufferedReader in;
+    private PrintWriter response;
     private final String[] files = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
-    public Connection() {
+    public Connection() throws IOException {
         Board board = new Board();
         state = board.getBoardState();
         this.w = new Player(Color.WHITE, state);
         this.b = new Player(Color.BLACK, state);
         engine = new MinmaxAB(b, w, state, 2, true);
+        this.in = new BufferedReader(new InputStreamReader(System.in));
+        this.response = new PrintWriter(System.out);
         runXBoard();
 
     }
@@ -42,11 +47,13 @@ public class Connection {
         Boolean start = false;
         while (!start) {
             String command = input.nextLine();
+
             if (command.startsWith("new")) {
+
                 if (engine.getPlayer().getColor().equals(Color.WHITE)) {
                     Move enginemove = engine.getMove();
                     enginemove.execute();
-                    System.out.append("move " + enginemove +"\n");
+                    System.out.append("move " + enginemove + "\n");
                     System.out.flush();
                     w.updatePlayer();
                     b.updatePlayer();
@@ -57,26 +64,89 @@ public class Connection {
         }
         while (true) {
 
-            String command = input.nextLine();
-            if (command.startsWith("usermove")) {
-
-                Move move = convertToMove(command.substring(9), w);
-                move.execute();
-                w.updatePlayer();
-                b.updatePlayer();
-
-                Move enginemove = engine.getMove();
-                enginemove.execute();
-                System.out.append("move " + enginemove + "\n");
-                System.out.flush();
-
-                w.updatePlayer();
-                b.updatePlayer();
+            String command = "?";
+            try {
+                command = this.in.readLine();
+           
+            } catch (Exception e) {
             }
+
             if (command.equals("quit")) {
                 break;
+            } else if (command.startsWith("sd")) {
+                //do nothing for now
+            } else if (command.startsWith("st")) {
+                //do nothing for now
+            } else if (command.startsWith("variant")) {
+                //do nothing for now
+            } else if (command.startsWith("force")) {
+                // do nothing for now
+            } else if (command.startsWith("go")) {
+                //do nothing for now
+            } else if (command.startsWith("level")) {
+                //do nothing for now
+            } else if (command.startsWith("time")) {
+                //do nothing for now
+            } else if (command.startsWith("otim")) {
+                //do nothing for now
+            } else if (command.startsWith("draw")) {
+                //do nothing for now
+            } else if (command.startsWith("result")) {
+                //do nothing for now
+            } else if (command.startsWith("hint")) {
+                //do nothing for now
+            } else if (command.startsWith("white")) {
+                //do nothing for now
+            } else if (command.startsWith("black")) {
+                //do nothing for now
+            } else if (command.startsWith("?")) {
+                //do nothing for now
+            } else if (command.startsWith("edit")) {
+                //do nothing for now
+            } else if (command.startsWith("bk")) {
+                //do nothing for now
+            } else if (command.startsWith("undo")) {
+                //do nothing for now
+            } else if (command.startsWith("remove")) {
+                //do nothing for now
+            } else if (command.startsWith("hard")) {
+                //do nothing for now
+            } else if (command.startsWith("easy")) {
+                //do nothing for now
+            } else if (command.startsWith("post")) {
+                //do nothing for now
+            } else if (command.startsWith("nopost")) {
+                //do nothing for now
+            } else if (command.startsWith("name")) {
+                //do nothing for now
+            } else if (command.startsWith("rating")) {
+                //do nothing for now
+            } else if (command.startsWith("computer")) {
+                //do nothing for now
+            } else if (command.startsWith("analyze")) {
+
+            } else {
+
+                Move move = convertToMove(command.substring(0, 4), w);
+                if (move != null) {
+                    move.execute();
+                    w.updatePlayer();
+                    b.updatePlayer();
+
+                    Move enginemove = engine.getMove();
+                    enginemove.execute();
+                    w.updatePlayer();
+                    b.updatePlayer();
+
+                    response.write("move " + enginemove + "\n");
+                    response.flush();
+
+                }
+
             }
+
         }
+
     }
 
     private Move convertToMove(String input, Player player) {
